6266d6b0404b044f0969c39409a21f741a6bedba
melgor <melgor89@gmail.com>
2016-10-25 08:15:35 +0200

    Change to reshape in interference

--- caffe-master/src/caffe/layers/multi_stage_meanfield.cpp
+++ caffe-master/src/caffe/layers/multi_stage_meanfield.cpp
@@ -155,7 +155,120 @@ void MultiStageMeanfieldLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bot
 template <typename Dtype>
 void MultiStageMeanfieldLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-	// Do nothing.
+  count_ = bottom[0]->count();
+  num_ = bottom[0]->num();
+  channels_ = bottom[0]->channels();
+  height_ = bottom[0]->height();
+  width_ = bottom[0]->width();
+  num_pixels_ = height_ * width_;
+  
+  top[0]->Reshape(num_, channels_, height_, width_);
+   // Initialize the parameters that will updated by backpropagation.
+  if (this->blobs_.size() > 0) {
+//     LOG(INFO) << "Multimeanfield layer skipping parameter initialization.";
+  } else {
+
+    this->blobs_.resize(3);// blobs_[0] - spatial kernel weights, blobs_[1] - bilateral kernel weights, blobs_[2] - compatability matrix
+
+    // Allocate space for kernel weights.
+    this->blobs_[0].reset(new Blob<Dtype>(1, 1, channels_, channels_));
+    this->blobs_[1].reset(new Blob<Dtype>(1, 1, channels_, channels_));
+
+    caffe_set(channels_ * channels_, Dtype(0.), this->blobs_[0]->mutable_cpu_data());
+    caffe_set(channels_ * channels_, Dtype(0.), this->blobs_[1]->mutable_cpu_data());
+
+    // Initialize the kernels weights. The two files spatial.par and bilateral.par should be available.
+    FILE * pFile;
+    pFile = fopen("spatial.par", "r");
+    CHECK(pFile) << "The file 'spatial.par' is not found. Please create it with initial spatial kernel weights.";
+    for (int i = 0; i < channels_; i++) {
+      fscanf(pFile, "%lf", &this->blobs_[0]->mutable_cpu_data()[i * channels_ + i]);
+    }
+    fclose(pFile);
+
+    pFile = fopen("bilateral.par", "r");
+    CHECK(pFile) << "The file 'bilateral.par' is not found. Please create it with initial bilateral kernel weights.";
+    for (int i = 0; i < channels_; i++) {
+      fscanf(pFile, "%lf", &this->blobs_[1]->mutable_cpu_data()[i * channels_ + i]);
+    }
+    fclose(pFile);
+
+    // Initialize the compatibility matrix.
+    this->blobs_[2].reset(new Blob<Dtype>(1, 1, channels_, channels_));
+    caffe_set(channels_ * channels_, Dtype(0.), this->blobs_[2]->mutable_cpu_data());
+
+    // Initialize it to have the Potts model.
+    for (int c = 0; c < channels_; ++c) {
+      (this->blobs_[2]->mutable_cpu_data())[c * channels_ + c] = Dtype(-1.);
+    }
+  }
+  
+  // Initialize the spatial lattice. This does not need to be computed for every image because we use a fixed size.
+  float spatial_kernel[2 * num_pixels_];
+  compute_spatial_kernel(spatial_kernel);
+  spatial_lattice_.reset(new ModifiedPermutohedral());
+  spatial_lattice_->init(spatial_kernel, 2, num_pixels_);
+
+  // Calculate spatial filter normalization factors.
+  norm_feed_.reset(new Dtype[num_pixels_]);
+  caffe_set(num_pixels_, Dtype(1.0), norm_feed_.get());
+  spatial_norm_.Reshape(1, 1, height_, width_);
+  Dtype* norm_data = spatial_norm_.mutable_cpu_data();
+  spatial_lattice_->compute(norm_data, norm_feed_.get(), 1);
+  for (int i = 0; i < num_pixels_; ++i) {
+    norm_data[i] = 1.0f / (norm_data[i] + 1e-20f);
+  }
+
+  // Allocate space for bilateral kernels. This is a temporary buffer used to compute bilateral lattices later.
+  // Also allocate space for holding bilateral filter normalization values.
+  bilateral_kernel_buffer_.reset(new float[5 * num_pixels_]);
+  bilateral_norms_.Reshape(num_, 1, height_, width_);
+  
+    // Allocate space for bilateral kernels. This is a temporary buffer used to compute bilateral lattices later.
+  // Also allocate space for holding bilateral filter normalization values.
+  bilateral_kernel_buffer_.reset(new float[5 * num_pixels_]);
+  bilateral_norms_.Reshape(num_, 1, height_, width_);
+
+  // Configure the split layer that is used to make copies of the unary term. One copy for each iteration.
+  // It may be possible to optimize this calculation later.
+  split_layer_bottom_vec_.clear();
+  split_layer_bottom_vec_.push_back(bottom[0]);
+
+  split_layer_top_vec_.clear();
+
+  split_layer_out_blobs_.resize(num_iterations_);
+  for (int i = 0; i < num_iterations_; i++) {
+    split_layer_out_blobs_[i].reset(new Blob<Dtype>());
+    split_layer_top_vec_.push_back(split_layer_out_blobs_[i].get());
+  }
+
+  LayerParameter split_layer_param;
+  split_layer_.reset(new SplitLayer<Dtype>(split_layer_param));
+  split_layer_->SetUp(split_layer_bottom_vec_, split_layer_top_vec_);
+
+  // Make blobs to store outputs of each meanfield iteration. Output of the last iteration is stored in top[0].
+  // So we need only (num_iterations_ - 1) blobs.
+  iteration_output_blobs_.resize(num_iterations_ - 1);
+  for (int i = 0; i < num_iterations_ - 1; ++i) {
+    iteration_output_blobs_[i].reset(new Blob<Dtype>(num_, channels_, height_, width_));
+  }
+
+  // Make instances of MeanfieldIteration and initialize them.
+  meanfield_iterations_.resize(num_iterations_);
+  for (int i = 0; i < num_iterations_; ++i) {
+    meanfield_iterations_[i].reset(new MeanfieldIteration<Dtype>());
+    meanfield_iterations_[i]->OneTimeSetUp(
+        split_layer_out_blobs_[i].get(), // unary terms
+        (i == 0) ? bottom[1] : iteration_output_blobs_[i - 1].get(), // softmax input
+        (i == num_iterations_ - 1) ? top[0] : iteration_output_blobs_[i].get(), // output blob
+        spatial_lattice_, // spatial lattice
+        &spatial_norm_); // spatial normalization factors.
+  }
+
+  this->param_propagate_down_.resize(this->blobs_.size(), true);
+  
+  
+	
 }
 
 
